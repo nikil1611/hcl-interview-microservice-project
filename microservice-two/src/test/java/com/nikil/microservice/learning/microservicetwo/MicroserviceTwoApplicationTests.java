b8392b96481cc455cab9a4ea774b9164185cc620
nikil1611 <nikiljaincse@gmail.com>
2024-03-02 14:06:50 +0530

    initial project setup

---
+++ microservice-two/src/test/java/com/nikil/microservice/learning/microservicetwo/MicroserviceTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikil.microservice.learning.microservicetwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
